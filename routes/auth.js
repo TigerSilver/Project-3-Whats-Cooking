d1e060dfef9ad320a8f32349d0b56b7537ce26c6
Tiago Silva <tiagosilva94@outlook.com>
2019-10-03 13:14:09 +0100

    Add routing tryouts

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const { Router } = require("express");
+const router = Router();
+const User = require("./../models/user");
+
+//POST: Sign-up
+
+//POST: Login
+
+//POST: Log-out
+
+//POST: Logged In
