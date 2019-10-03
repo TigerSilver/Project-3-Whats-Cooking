5cf96fe9f2f5b6f9ae53855592675e354261dba2
Daniel Mendes Valero <dmendesvalero@gmail.com>
2019-10-03 17:42:52 +0100

    error fixed

--- models/user/index.js
+++ models/user/index.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   lastName: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   username: {
     type: String,
-    required: true,
+    // required: true,
     trim: true,
     unique: true
   },
